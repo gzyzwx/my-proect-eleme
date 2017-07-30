ac6f6666ed5333319a6148bae96d4dbbbdb9a068
gzyzwx <357448012@qq.com>
2017-07-30 12:07:21 +0800

    提交不上去？测试下

--- config/index.js
+++ config/index.js
@@ -15,7 +15,7 @@ module.exports = {
     // npm install --save-dev compression-webpack-plugin
     productionGzip: false,
     productionGzipExtensions: ['js', 'css'],
-    port: 9000
+    port: 9090
   },
   dev: {
     env: require('./dev.env'),
